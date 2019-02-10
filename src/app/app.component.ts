feb45f31d743b76f2000f2542155b982032bf9e5
Jason <jasonmoramora@outlook.com>
2019-02-10 16:12:08 -0500

    Mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Aplicacion con angular';
+  autor = 'Jaison Mora';
+  hobbies = [
+    'Entrenar Judo',
+    'Jugar Dota',
+    'Investigar sobre devs'
+  ];
+  edad = 17;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
